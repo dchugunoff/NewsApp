9281001fe43a7fdef13d5a4f9acfc92dab804e5b
dchugunoff <yolvissplay@gmail.com>
2023-05-07 15:19:22 +0300

    create NewsService

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "b7c28bd5340e46a4836d1055b658b3a1"
+    }
+}
