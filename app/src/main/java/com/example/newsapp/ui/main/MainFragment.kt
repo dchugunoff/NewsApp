dd192cb476dfc9516d1d6e28870f33dc92ce98be
dchugunoff <yolvissplay@gmail.com>
2023-05-07 18:48:55 +0300

    create RecyclerView and Adapter for MainFragment.kt

--- app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/main/MainFragment.kt
@@ -1,13 +1,18 @@
 package com.example.newsapp.ui.main
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.FragmentMainBinding
+import com.example.newsapp.ui.adapters.NewsAdapter
+import com.example.newsapp.utils.Resource
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
 @AndroidEntryPoint
@@ -16,6 +21,7 @@ class MainFragment : Fragment() {
     private val mBinding get() = _binding!!
 
     private val viewModel by viewModels<MainViewModel>()
+    lateinit var newsAdapter: NewsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +33,33 @@ class MainFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.all
+        initAdapter()
+        viewModel.newsLiveData.observe(viewLifecycleOwner) { responce ->
+            when (responce) {
+                is Resource.Success -> {
+                    pag_progress_bar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        newsAdapter.differ.submitList(it.articles)
+                    }
+                }
+                is Resource.Loading -> {
+                    pag_progress_bar.visibility = View.VISIBLE
+                }
+                is Resource.Error -> {
+                    pag_progress_bar.visibility = View.INVISIBLE
+                    responce.data?.let {
+                        Log.e("checkData", "MainFragment: error $it")
+                    }
+                }
+            }
+        }
+    }
+
+    private fun initAdapter() {
+        newsAdapter = NewsAdapter()
+        news_rv.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
