041c26b5d2670c0baa8db3d733db5374596d7864
dchugunoff <yolvissplay@gmail.com>
2023-05-08 20:36:17 +0300

    added detail fragment logic

--- app/src/main/java/com/example/newsapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/example/newsapp/ui/detail/DetailFragment.kt
@@ -1,11 +1,17 @@
 package com.example.newsapp.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.newsapp.R
+import android.webkit.URLUtil
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.example.newsapp.databinding.FragmentDetailBinding
 
 
@@ -13,6 +19,7 @@ class DetailFragment : Fragment() {
 
     private var _binding: FragmentDetailBinding? = null
     private val mBinding get() = _binding!!
+    private val bundleArgs: DetailFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +27,42 @@ class DetailFragment : Fragment() {
     ): View? {
         _binding = FragmentDetailBinding.inflate(inflater, container, false)
         return mBinding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val articleArg = bundleArgs.article
+        articleArg.let { article ->
+            article.urlToImage.let {
+                Glide.with(this).load(article.urlToImage).into(mBinding.headerImage)
+            }
+            mBinding.headerImage.clipToOutline= true
+            mBinding.articleDetailTitle.text = article.title
+            mBinding.descriptionText.text = article.description
+
+            mBinding.btnVisitSite.setOnClickListener {
+                try {
+                    Intent()
+                        .setAction(Intent.ACTION_VIEW)
+                        .addCategory(Intent.CATEGORY_BROWSABLE)
+                        .setData(Uri.parse(takeIf { URLUtil.isValidUrl(article.url) }
+                            ?.let {
+                                article.url
+                            } ?: "https://google.com"))
+                        .let {
+                            ContextCompat.startActivity(requireContext(), it, null)
+                        }
+                } catch (e: Exception) {
+                    Toast.makeText(
+                        context,
+                        "The device doesnt have any browser to view the document!",
+                        Toast.LENGTH_SHORT
+                    )
+                }
+            }
+        }
     }
 
 }
